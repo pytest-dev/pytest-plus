a0464947c753864b75ec02fcc16cb9e171e1ebc3
Sorin Sbarnea <ssbarnea@redhat.com>
2023-10-17 16:08:41 +0100

    Prevent duplicate test names (#18)

--- src/pytest_plus/__init__.py
+++ src/pytest_plus/__init__.py
@@ -2,21 +2,21 @@
 
 from __future__ import print_function
 import os
+from typing import List
+
 import pytest
-import sys
 
 
-@pytest.hookimpl(hookwrapper=True)
-def pytest_terminal_summary(terminalreporter, exitstatus, config):
+def pytest_sessionfinish(session, exitstatus):
     """Assure passed test match PYTEST_REQPASS value.
 
     Assures that pytest returns an error code when the number of expected passed
     tests does not match the PYTEST_REQPASS value.  When not defined or zero
     that functionality is ignored.
     """
-    yield
+    terminalreporter = session.config.pluginmanager.get_plugin("terminalreporter")
     req_passed = int(os.environ.get("PYTEST_REQPASS", "0"))
-    if req_passed and not config.option.collectonly:
+    if req_passed and not session.config.option.collectonly:
         passed = 0
         for x in terminalreporter.stats.get("passed", []):
             if x.when == "call" and x.outcome == "passed":
@@ -28,4 +28,22 @@ def pytest_terminal_summary(terminalreporter, exitstatus, config):
                     passed, req_passed
                 )
             )
-            sys.exit(1)
+            session.exitstatus = 1
+
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_collection_modifyitems(items: List[pytest.Item]) -> None:
+    errors = []
+    names = {}
+    for item in items:
+        base_name = item.name.split("[")[0]
+        if base_name not in names:
+            names[base_name] = item.location
+        elif item.location[:2] != names[base_name][:2]:
+            error = f"Duplicate test name '{base_name}', found at {item.location[0]}:{item.location[1]} and {names[base_name][0]}:{names[base_name][1]}"
+            if error not in errors:
+                errors.append(error)
+    if errors:
+        raise pytest.UsageError(
+            f"Failed run due to following issues being identified:\n{os.linesep.join(errors)}"
+        )
--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,2 +1,31 @@
-def test_one():
-    pass
+"""Tests."""
+import pytest
+
+pytest_plugins = ["pytester"]
+
+
+def test_duplicate_test_name(pytester: pytest.Pytester) -> None:
+    """Validates that we can detect duplicate test names."""
+    p1 = pytester.makepyfile(
+        test_one="""
+            def test_a():
+                assert True
+            """,
+    )
+    p2 = pytester.makepyfile(
+        test_two="""
+            def test_a():
+                assert True
+            """,
+    )
+
+    result = pytester.runpytest_inprocess(p1, p2)
+    assert (
+        result.errlines[0]
+        == "ERROR: Failed run due to following issues being identified:"
+    )
+    assert (
+        result.errlines[1]
+        == "Duplicate test name 'test_a', found at test_two.py:0 and test_one.py:0"
+    )
+    assert result.ret == 4
